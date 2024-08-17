4d50ece8f4b07d1697e98173fc89eb96667e578b
KAkos27 <akoskosztolanyi@gmail.com>
2024-08-17 16:35:40 +0200

    igen

---
+++ src/components/Product.jsx
@@ -0,0 +1,19 @@
+import { CartContext } from "../store/shopping-cart-context.jsx";
+
+export default function Product({ id, image, title, price, description }) {
+  return (
+    <article className="product">
+      <img src={image} alt={title} />
+      <div className="product-content">
+        <div>
+          <h3>{title}</h3>
+          <p className="product-price">${price}</p>
+          <p>{description}</p>
+        </div>
+        <p className="product-actions">
+          <button onClick={() => onAddToCart(id)}>Add to Cart</button>
+        </p>
+      </div>
+    </article>
+  );
+}
